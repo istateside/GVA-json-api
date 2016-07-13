3e8b1cd2b4978654102379dba5099dfb3be1721c
istateside <kevinrfleischman@gmail.com>
2016-07-13 10:58:12 -0400

    includes date_as_utc in serialized attributes

--- app/serializers/incident_serializer.rb
+++ app/serializers/incident_serializer.rb
@@ -1,3 +1,3 @@
 class IncidentSerializer < ActiveModel::Serializer
-  attributes :id, :state, :city, :address, :numKilled, :numInjured, :uid
+  attributes :id, :state, :city, :address, :numKilled, :numInjured, :uid, :date_as_utc
 end
